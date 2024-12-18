4fe3f79c0d75e5102a49ede7f2064620f4f7c941
PseudoJoker-1 <armaha2302@gmail.com>
2024-12-18 21:49:53 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,11 +6,19 @@ const profile = document.querySelector('.profile')
 const profileName = document.querySelector('.profile-name')
 const cartList = document.getElementById('cartList');
 const totalSumP = document.querySelector('.TotalSum')
+const productsBtn = document.querySelector('.Products')
+const header = document.querySelector('header');
+const ProductsSection = document.querySelector('.ProductsSect')
+
+
+let foundOne = false
+let OnProducts = false
 let totalsum = 0
-let cart = []
+let allCart = []
 let products = [];
 prevButton.style.opacity = 0
 let currentIndex = 0;
+
 const url = 'https://fakestoreapi.com/products/'; 
 const options = { 
  method: 'GET', 
@@ -104,43 +112,156 @@ function addToCart(product) {
         const userName2 = JSON.parse(localStorage.getItem('usersData'))
         console.log(userName2.username)
         profileName.textContent = userName2.username
-        cart.push(product);
-        updateCartDisplay();
-    }catch{
-        alert('Вы не зарегистрированы!')
+        product['amount'] = 1
+        Array.from(allCart).forEach(child => {
+            if(child.title === product.title){
+                console.log('yes')
+                var res = allCart.find(item => item.title === child.title)
+                res.amount += 1
+                console.log(res)
+                
+            }
+        });
+        
+        allCart.push(product);
+        console.log(allCart)
+        updateCartDisplay(allCart);
+        totalsum += product.price
+        totalSumP.textContent = `Total sum: ${totalsum.toFixed(2)}₱`
+    }catch(error){
+        alert(console.error(error))
     }
     
 
 }
 
 function removeFromCart(product) {
-    cart = cart.filter(item => item !== product);
-    updateCartDisplay();
+    allCart = allCart.filter(item => item !== product);
+    console.log('All cart:',allCart)
+    totalsum -= product.price
+    console.log(totalsum)
+    totalSumP.textContent = `Total sum: ${totalsum.toFixed(2)}₱`
+    updateCartDisplay(allCart)
 }
 
-function updateCartDisplay() {
-    cart.forEach(item => {
-        const cartItem = document.createElement('div');
-        cartItem.className = 'cart-item';
+function AddItem(item1){
+    var result = allCart.find(item => item.title === item1.title)
+    result.amount += 1
+    updateCartDisplay(allCart)
+}
 
-        const title = document.createElement('p');
-        title.textContent = item.title;
+function RemoveItem(item2){
+    var result = allCart.find(item => item.title === item2.title)
+    result.amount -= 1
+    updateCartDisplay(allCart)
+}
 
-        const price = document.createElement('p');
-        price.textContent = `₱ ${item.price}`;
-
-        const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
-        removeButton.addEventListener('click', () => removeFromCart(item));
-
-        totalsum += item.price
-        totalSumP.textContent = `Total sum: ${totalsum}₱`
-        cartItem.appendChild(title);
-        cartItem.appendChild(price);
-        cartItem.appendChild(removeButton);
-
-        cartList.appendChild(cartItem);
+function checkDuplicatesAndRemove(arr, key) {
+    const seen = new Set(); 
+    const uniqueItems = arr.filter(item => {
+        if (seen.has(item[key])) {
+            return false; 
+        }
+        seen.add(item[key]);
+        return true; 
     });
+
+    if (uniqueItems.length < arr.length) {
+        updateCartDisplay(uniqueItems)
+    } else {
+        console.log('No duplicates found');
+        return null
+    }
+    return uniqueItems
+}
+
+function updateCartDisplay(cartarg) {
+    foundOne = false
+    Array.from(cartList.children).forEach(child => {
+        if (child.tagName !== 'P' && child.tagName !== 'BUTTON') {
+            child.remove();
+        }
+
+    });
+    console.log(allCart)
+
+    var dupli = checkDuplicatesAndRemove(allCart,'id')
+    if(allCart.length !== 0){
+        if (allCart.length !== dupli.length) {
+            console.log('Duplicates were found and removed!');
+            dupli.forEach(item => {
+                const cartItem = document.createElement('div');
+                cartItem.className = 'cart-item';
+                const title = document.createElement('p');
+                title.textContent = item.title;
+                title.className = 'Product-title'
+                const price = document.createElement('p');
+                price.textContent = `₱ ${item.price}`;
+                const AddRemoveMore = document.createElement('div')
+                AddRemoveMore.className = 'AddRemoveMore'
+                const Amount = document.createElement('p')
+                Amount.className = 'Amount'
+                Amount.style.fontSize = '15px'
+                Amount.textContent = item.amount
+                const Addbtn = document.createElement('button')
+                Addbtn.className = 'Addmore'
+                Addbtn.textContent = '+'
+                Addbtn.addEventListener('click', () => AddItem(item))
+                const Removebtn = document.createElement('button')
+                Removebtn.className = 'Removemore'
+                Removebtn.textContent = '-'
+                Removebtn.addEventListener('click', () => RemoveItem(item))
+                AddRemoveMore.appendChild(Addbtn)
+                AddRemoveMore.appendChild(Amount)
+                AddRemoveMore.appendChild(Removebtn)      
+                const removeButton = document.createElement('button');
+                removeButton.textContent = 'Remove';
+                removeButton.addEventListener('click', () => removeFromCart(item));
+                cartItem.appendChild(title);
+                cartItem.appendChild(price);
+                cartItem.appendChild(AddRemoveMore)
+                cartItem.appendChild(removeButton);
+                cartList.appendChild(cartItem);
+            })
+        } else {
+            console.log('No duplicates found.');
+        
+            cartarg.forEach(item => {
+                const cartItem = document.createElement('div');
+                cartItem.className = 'cart-item';
+                const title = document.createElement('p');
+                title.textContent = item.title;
+                title.className = 'Product-title'
+                const price = document.createElement('p');
+                price.textContent = `₱ ${item.price}`;
+                const AddRemoveMore = document.createElement('div')
+                AddRemoveMore.className = 'AddRemoveMore'
+                const Amount = document.createElement('p')
+                Amount.className = 'Amount'
+                Amount.style.fontSize = '15px'
+                Amount.textContent = item.amount
+                const Addbtn = document.createElement('button')
+                Addbtn.className = 'Addmore'
+                Addbtn.textContent = '+'
+                Addbtn.addEventListener('click', () => AddItem(item))
+                const Removebtn = document.createElement('button')
+                Removebtn.className = 'Removemore'
+                Removebtn.textContent = '-'
+                Removebtn.addEventListener('click', () => RemoveItem(item))
+                AddRemoveMore.appendChild(Addbtn)
+                AddRemoveMore.appendChild(Amount)
+                AddRemoveMore.appendChild(Removebtn)      
+                const removeButton = document.createElement('button');
+                removeButton.textContent = 'Remove';
+                removeButton.addEventListener('click', () => removeFromCart(item));
+                cartItem.appendChild(title);
+                cartItem.appendChild(price);
+                cartItem.appendChild(AddRemoveMore)
+                cartItem.appendChild(removeButton);
+                cartList.appendChild(cartItem);
+            })
+        }
+    }
 }
 
 
@@ -218,6 +339,106 @@ function sideBarActive(){
     else{
         cartList.style.transform = `translateX(${-400}px)`
     }
+}
+
+function buyFunc(){
+    if(cart.length !== 0){
+        alert('Successful!')
+        totalsum -= totalsum
+        totalSumP.textContent = `Total sum: ${totalsum}₱`
+        Array.from(cartList.children).forEach(child => {
+            if (child.tagName !== 'P' && child.tagName !== 'BUTTON') {
+                child.remove();
+            }
+        });
+
+    }
+    
+}
+
+function ShowProducts(){
+    Array.from(document.body.children).forEach(child => {
+        if (child.tagName !== 'HEADER' && child.tagName !== 'FOOTER' && child.className !== 'ProductsSect' && child.className !== 'products-container' && child.tagName !== 'SCRIPT') {
+
+            child.style.display = 'none';
+        }
+    });
+    Array.from(header.children).forEach(child => {
+        if (!child.classList.contains('head')) {
+          child.style.display = 'none'; 
+        }
+      });
+    
+    OnProducts = true
+
+    
+    fetch(url, options)
+    .then(data => data.json())
+    .then(answer2 => {
+      console.log(answer2);
+
+      const productsContainer = document.createElement('div');
+      productsContainer.classList.add('products-container');
+      ProductsSection.append(productsContainer);
+
+      answer2.forEach(element => {
+        const block = document.createElement('div');
+        block.classList.add('products-List');
+
+        const image = document.createElement('img');
+        image.src = element.image;
+
+        const nameBrand = document.createElement('h2');
+        nameBrand.textContent = element.title;
+        nameBrand.classList.add('brandname2');
+
+        const price = document.createElement('p');
+        price.classList.add('price2');
+        price.textContent = `₱ ${element.price}`;
+
+        const btn = document.createElement('button');
+        btn.classList.add('buybtn2')
+        btn.textContent = 'Buy'
+        btn.addEventListener('click', () => {
+            addToCart(element)
+        });
+        
+        block.append(image);
+        block.append(nameBrand);
+        block.append(price);
+        block.append(btn)
+        productsContainer.append(block);
+      });
+    });
+    
+}
+
+
+
+function ShowHome(){
+    if(OnProducts === true){
+        Array.from(document.body.children).forEach(child => {
+            if (child.tagName !== 'HEADER' && child.tagName !== 'FOOTER' && child.className !== 'ProductsSect' && child.className !== 'products-container' && child.tagName !== 'SCRIPT') {
+                if(child.className === 'about-us'){
+                    child.style.display = 'flex'
+                }
+                else{
+                    child.style.display = 'block';
+                }
+                OnProducts = false
+
+            }
+        });
+        Array.from(header.children).forEach(child => {
+            if (!child.classList.contains('head')) {
+              child.style.display = 'block'; 
+            }
+          });
+
+          ProductsSection.innerHTML = '';
+
+    }
+
 }
 
 
